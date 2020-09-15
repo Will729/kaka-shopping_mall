0991dc4d925109f32d1a2809e0f90cddfaee187b
Will729 <81878294@qq.com>
2020-09-15 12:44:14 +0800

    shopping mall pro start

---
+++ 1.py
@@ -0,0 +1,15 @@
+from flask import Flask
+class Config:
+    SECRET_KEY = 'mrsoft'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    @staticmethod
+    def init_app(app):
+        '''初始化配置文件'''
+        pass
+
+app = Flask(__name__)
+
+
+
+print(Config.init_app(app))
